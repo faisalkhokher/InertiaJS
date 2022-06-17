5bb26106682924f12e17a19525d839f5a72384a8
faisalkhokher50 <faisalkhokher50@gmail.com>
2022-06-17 18:08:08 +0500

    Implement redis

--- config/database.php
+++ config/database.php
@@ -117,30 +117,22 @@ return [
     |
     */
 
-    'redis' => [
-
-        'client' => env('REDIS_CLIENT', 'predis'),
-
+   'redis' => [
+    'client' => 'predis',
+    'cluster' => true,
     'options' => [
-        'cluster' => env('REDIS_CLUSTER', 'redis'),
-        'prefix' => env('REDIS_PREFIX', ''),
-    ],
-
-    'default' => [
-        'url' => env('REDIS_URL'),
-        'host' => env('REDIS_HOST', '127.0.0.1'),
-        'password' => env('REDIS_PASSWORD', null),
-        'port' => env('REDIS_PORT', '6379'),
-        'database' => env('REDIS_DB', '0'),
-    ],
-
-    'cache' => [
-        'url' => env('REDIS_URL'),
-        'host' => env('REDIS_HOST', '127.0.0.1'),
-        'password' => env('REDIS_PASSWORD', null),
-        'port' => env('REDIS_PORT', '6379'),
-        'database' => env('REDIS_CACHE_DB', '1'),
-    ],
-    ],
+        'cluster' => 'redis',
+            'default' => [
+                'host' => env('REDIS_HOST', 'localhost'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => 0,
+            ],
+            'options' => [
+                'cluster' => 'redis',
+                'parameters' => ['password' => env('REDIS_PASSWORD', null)],
+            ],
+     ]
+   ]
 
 ];
